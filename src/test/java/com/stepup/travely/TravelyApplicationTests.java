52652fd63972b585222ac93e1ac0d3d370b98182
jsween5723 <jsween5723@gmail.com>
2023-11-10 10:00:55 +0900

    feat(init): 프로젝트 생성

---
+++ src/test/java/com/stepup/travely/TravelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepup.travely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
